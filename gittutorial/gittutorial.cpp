0a08fa05eccde821b348ce835cb2f27e264f0118
Li-Chun-Chen <ahm12345@gmail.com>
2018-03-16 11:14:27 +0800

    add

---
+++ gittutorial/gittutorial.cpp
@@ -0,0 +1,24 @@
+#include "stdafx.h"
+#include <string>
+#include <iostream>
+using namespace System;
+
+bool Guess(int number) {
+	static int target = -1;
+	if (number == target) {
+		std::cout << "Correct !!";
+		target = -1;
+		return true;
+	}
+	else if (number > target) std::cout << "Smaller" << std::endl;
+	else if (number < target) std::cout << "Bigger" << std::endl;
+}
+int main(array<System::String ^> ^args)
+{
+	int guess;
+	do {
+		std::cout << "Choose a number between 1 - 100 :";
+		std::cin >> guess;
+	} while (!Guess(guess));
+	return 0;
+}
